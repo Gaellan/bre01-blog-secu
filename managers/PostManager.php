bd7db55911207248bf5389274d5e07f44ecf1c6f
Gaellan <doucet.mari@gmail.com>
2024-02-07 11:28:31 +0100

    step 4 : PostManager

--- managers/PostManager.php
+++ managers/PostManager.php
@@ -14,16 +14,69 @@ class PostManager extends AbstractManager
 
     public function findLatest() : array
     {
-        return [];
+        $um = new UserManager();
+
+        $query = $this->db->prepare('SELECT * FROM posts ORDER BY created_at LIMIT 4');
+        $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        $posts = [];
+
+        foreach($result as $item)
+        {
+            $user = $um->findOne($item["author"]);
+            $post = new Post($item["title"], $item["excerpt"], $item["content"], $user, DateTime::createFromFormat('Y-m-d H:i:s', $item["created_at"]));
+            $post->setId($item["id"]);
+            $posts[] = $post;
+        }
+
+        return $posts;
     }
 
     public function findOne(int $id) : ? Post
     {
+        $um = new UserManager();
+
+        $query = $this->db->prepare('SELECT * FROM posts WHERE id=:id');
+
+        $parameters = [
+            "id" => $id
+        ];
+
+        $query->execute($parameters);
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+
+        if($result)
+        {
+            $user = $um->findOne($result["author"]);
+            $post = new Post($result["title"], $result["excerpt"], $result["content"], $user, DateTime::createFromFormat('Y-m-d H:i:s', $result["created_at"]));
+            $post->setId($result["id"]);
+
+            return $post;
+        }
+
         return null;
     }
 
     public function findByCategory(int $categoryId) : array
     {
-        return [];
+        $um = new UserManager();
+
+        $query = $this->db->prepare('SELECT posts.* FROM posts JOIN posts_categories ON posts_categories.post_id=posts.id WHERE posts_categories.category_id=:category_id');
+        $parameters = [
+            "category_id" => $categoryId
+        ];
+        $query->execute($parameters);
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        $posts = [];
+
+        foreach($result as $item)
+        {
+            $user = $um->findOne($item["author"]);
+            $post = new Post($item["title"], $item["excerpt"], $item["content"], $user, DateTime::createFromFormat('Y-m-d H:i:s', $item["created_at"]));
+            $post->setId($item["id"]);
+            $posts[] = $post;
+        }
+
+        return $posts;
     }
 }
