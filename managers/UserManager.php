56703b488aa97087ace9da995a976830b68e93d9
Gaellan <doucet.mari@gmail.com>
2024-02-07 11:43:08 +0100

    step 4 : UserManager

--- managers/UserManager.php
+++ managers/UserManager.php
@@ -14,16 +14,64 @@ class UserManager extends AbstractManager
 
     public function findByEmail(string $email) : ? User
     {
+        $query = $this->db->prepare('SELECT * FROM users WHERE email=:email');
+
+        $parameters = [
+            "email" => $email
+        ];
+
+        $query->execute($parameters);
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+
+        if($result)
+        {
+            $user = new User($result["username"], $result["email"], $result["password"], $result["role"]);
+            $user->setId($result["id"]);
+
+            return $user;
+        }
+
         return null;
     }
 
     public function findOne(int $id) : ? User
     {
+        $query = $this->db->prepare('SELECT * FROM users WHERE id=:id');
+
+        $parameters = [
+            "id" => $id
+        ];
+
+        $query->execute($parameters);
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+
+        if($result)
+        {
+            $user = new User($result["username"], $result["email"], $result["password"], $result["role"]);
+            $user->setId($result["id"]);
+
+            return $user;
+        }
+
         return null;
     }
 
     public function create(User $user) : void
     {
+        $currentDateTime = date('Y-m-d H:i:s');
+
+        $query = $this->db->prepare('INSERT INTO users (id, username, email, password, role, created_at) VALUES (NULL, :username, :email, :password, :role, :created_at)');
+        $parameters = [
+            "username" => $user->getUsername(),
+            "password" => $user->getPassword(),
+            "email" => $user->getEmail(),
+            "role" => $user->getRole(),
+            "created_at" => $currentDateTime,
+        ];
+
+        $query->execute($parameters);
+
+        $user->setId($this->db->lastInsertId());
 
     }
 }
